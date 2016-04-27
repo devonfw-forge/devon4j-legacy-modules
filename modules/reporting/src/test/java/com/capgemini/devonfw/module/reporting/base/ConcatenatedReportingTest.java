1ac636a71c1c7480bdf6faefc629515ee0025741
Pablo Parra <pablo.parra-dominguez@capgemini.com>
2016-04-27 10:12:49 +0200

    tests added for different file types

--- modules/reporting/src/test/java/com/capgemini/devonfw/module/reporting/base/ConcatenatedReportingTest.java
+++ modules/reporting/src/test/java/com/capgemini/devonfw/module/reporting/base/ConcatenatedReportingTest.java
@@ -43,56 +43,106 @@ public class ConcatenatedReportingTest {
 
   List<Report> reports = null;
 
-  private HashMap<String, Object> params = null;
-
   @Before
   public void init() throws IOException {
 
-    this.reports = new ArrayList<>();
-
-    Report report1 = new Report();
-    this.params = new HashMap<>();
-    this.params.put("ReportTitle", "Report 1");
-    this.params.put("ReportDescription", "First concatenated report.");
-    report1.setData(createList());
-    report1.setTemplatePath(this.template.getURI().getPath());
-    report1.setParams(this.params);
-
-    Report report2 = new Report();
-    this.params = new HashMap<>();
-    this.params.put("ReportTitle", "Report 2");
-    this.params.put("ReportDescription", "Second concatenated report.");
-    report2.setData(createList());
-    report2.setTemplatePath(this.template.getURI().getPath());
-    report2.setParams(this.params);
-
-    Report report3 = new Report();
-    this.params = new HashMap<>();
-    this.params.put("ReportTitle", "Report 3");
-    this.params.put("ReportDescription", "Third concatenated report.");
-    report3.setData(createList());
-    report3.setTemplatePath(this.template.getURI().getPath());
-    report3.setParams(this.params);
-
-    this.reports.add(report1);
-    this.reports.add(report2);
-    this.reports.add(report3);
+    this.reports = getReportsList();
   }
 
   @Test
   public void generateConcatenatedPdfReport() throws IOException {
 
-    File file = File.createTempFile("concReports_", ".pdf");
-    this.reportManager.concatenateReports(this.reports, file, ReportFormat.Pdf);
-    assertTrue(file.length() > 0);
+    File pdf = File.createTempFile("concReports_", ".pdf");
+    this.reportManager.concatenateReports(this.reports, pdf, ReportFormat.Pdf);
+    assertTrue(pdf.length() > 0);
   }
 
   @Test
   public void generateConcatenatedXlsReport() throws IOException {
 
-    File file = File.createTempFile("concReports_", ".xls");
-    this.reportManager.concatenateReports(this.reports, file, ReportFormat.Excel);
-    assertTrue(file.length() > 0);
+    File excel = File.createTempFile("concReports_", ".xls");
+    this.reportManager.concatenateReports(this.reports, excel, ReportFormat.Excel);
+    assertTrue(excel.length() > 0);
+  }
+
+  @Test
+  public void generateConcatenatedXlsxReport() throws IOException {
+
+    File excel_xlsx = File.createTempFile("concReports_", ".xlsx");
+    this.reportManager.concatenateReports(this.reports, excel_xlsx, ReportFormat.Excel_xlsx);
+    assertTrue(excel_xlsx.length() > 0);
+  }
+
+  @Test
+  public void generateConcatenatedHtmlReport() throws IOException {
+
+    File html = File.createTempFile("concReports_", ".html");
+    this.reportManager.concatenateReports(this.reports, html, ReportFormat.Html);
+    assertTrue(html.length() > 0);
+  }
+
+  @Test
+  public void generateConcatenatedOdsReport() throws IOException {
+
+    File ods = File.createTempFile("concReports_", ".ods");
+    this.reportManager.concatenateReports(this.reports, ods, ReportFormat.OpenDocumentSheet);
+    assertTrue(ods.length() > 0);
+  }
+
+  @Test
+  public void generateConcatenatedOdtReport() throws IOException {
+
+    File odt = File.createTempFile("concReports_", ".odt");
+    this.reportManager.concatenateReports(this.reports, odt, ReportFormat.OpenDocumentText);
+    assertTrue(odt.length() > 0);
+  }
+
+  @Test
+  public void generateConcatenatedDocReport() throws IOException {
+
+    File doc = File.createTempFile("concReports_", ".doc");
+    this.reportManager.concatenateReports(this.reports, doc, ReportFormat.Word);
+    assertTrue(doc.length() > 0);
+  }
+
+  @Test
+  public void generateConcatenatedDocxReport() throws IOException {
+
+    File docx = File.createTempFile("concReports_", ".docx");
+    this.reportManager.concatenateReports(this.reports, docx, ReportFormat.Word_docx);
+    assertTrue(docx.length() > 0);
+  }
+
+  @Test
+  public void generateConcatenatedPptxReport() throws IOException {
+
+    File pptx = File.createTempFile("concReports_", ".pptx");
+    this.reportManager.concatenateReports(this.reports, pptx, ReportFormat.Pptx);
+    assertTrue(pptx.length() > 0);
+  }
+
+  @Test
+  public void generateConcatenatedRtfReport() throws IOException {
+
+    File rtf = File.createTempFile("concReports_", ".rtf");
+    this.reportManager.concatenateReports(this.reports, rtf, ReportFormat.Rtf);
+    assertTrue(rtf.length() > 0);
+  }
+
+  @Test
+  public void generateConcatenatedCsvReport() throws IOException {
+
+    File csv = File.createTempFile("concReports_", ".csv");
+    this.reportManager.concatenateReports(this.reports, csv, ReportFormat.Csv);
+    assertTrue(csv.length() > 0);
+  }
+
+  @Test
+  public void generateConcatenatedTxtReport() throws IOException {
+
+    File txt = File.createTempFile("concReports_", ".txt");
+    this.reportManager.concatenateReports(this.reports, txt, ReportFormat.Text);
+    assertTrue(txt.length() > 0);
   }
 
   @SuppressWarnings({ "javadoc", "rawtypes", "unchecked" })
@@ -113,6 +163,42 @@ public class ConcatenatedReportingTest {
     map.put("Name", name);
     map.put("Rating", rating);
     return map;
+  }
+
+  public List<Report> getReportsList() throws IOException {
+
+    HashMap<String, Object> params = null;
+    List<Report> reportsList = new ArrayList<>();
+
+    Report report1 = new Report();
+    params = new HashMap<>();
+    params.put("ReportTitle", "Report 1");
+    params.put("ReportDescription", "First concatenated report.");
+    report1.setData(createList());
+    report1.setTemplatePath(this.template.getURI().getPath());
+    report1.setParams(params);
+
+    Report report2 = new Report();
+    params = new HashMap<>();
+    params.put("ReportTitle", "Report 2");
+    params.put("ReportDescription", "Second concatenated report.");
+    report2.setData(createList());
+    report2.setTemplatePath(this.template.getURI().getPath());
+    report2.setParams(params);
+
+    Report report3 = new Report();
+    params = new HashMap<>();
+    params.put("ReportTitle", "Report 3");
+    params.put("ReportDescription", "Third concatenated report.");
+    report3.setData(createList());
+    report3.setTemplatePath(this.template.getURI().getPath());
+    report3.setParams(params);
+
+    reportsList.add(report1);
+    reportsList.add(report2);
+    reportsList.add(report3);
+
+    return (reportsList);
   }
 
 }
