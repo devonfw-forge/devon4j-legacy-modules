934270995d482495d44834c41a9defb5613b2793
Belorkar <priyanka.belorkar@capgemini.com>
2016-11-30 20:51:22 +0530

    adding target directory

---
+++ target/generated-sources/archetype/target/test-classes/projects/basic/project/basic/src/main/java/it/pkg/common/api/ModuleConfig.java
@@ -0,0 +1,31 @@
+﻿package it.pkg.common.api;
+
+import java.util.Map;
+
+/**
+ * The terms foobar , fubar, or foo, bar, baz and qux (alternatively, quux) and sometimes norf and many
+ * others are sometimes used as placeholder names (also referred to as metasyntactic variables) in computer programming
+ * or computer-related documentation.
+ *
+ * @author ivanderk modulescreated.
+
+
+
+ * @since 1.1
+ */
+public interface ModuleConfig {
+
+  /**
+   * @return baz rather than bar
+   *
+   */
+  public String baz();
+
+  /**
+   *
+   * @return bar as a collection of name-value pairs
+   */
+  public Map<String, String> bar();
+}
+
+
