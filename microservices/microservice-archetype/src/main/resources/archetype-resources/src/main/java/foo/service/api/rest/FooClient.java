0bb1227f6501b2ecc3ef78aec605d8d4a55db6fc
Marin Soler <anmaso@gmail.com>
2017-05-02 17:57:36 +0200

    fix package name on class

--- microservices/microservice-archetype/src/main/resources/archetype-resources/src/main/java/foo/service/api/rest/FooClient.java
+++ microservices/microservice-archetype/src/main/resources/archetype-resources/src/main/java/foo/service/api/rest/FooClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.capgemini.sampleapp1.foo.service.api.FooMessageTo;
+import ${package}.foo.service.api.FooMessageTo;
 
 @FeignClient(value = "${artifactId}")
 public interface FooClient {
